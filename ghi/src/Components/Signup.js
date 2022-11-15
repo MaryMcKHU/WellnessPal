0ec6a293738dbdd55b805f9ea54486d1c1fedf78
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-11-15 15:26:37 -0800

    Updated sign up form

--- ghi/src/Components/Signup.js
+++ ghi/src/Components/Signup.js
@@ -30,55 +30,79 @@ export default function Signup() {
             </Modal.Header>
             <Modal.Body style={{width:500}}>
             <form>
-                <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                    <h2 style={{ fontWeight: "bold", fontSize:25, textAlign:'center' }}>
-                    Welcome to Wellness Pal
-                    </h2>
-                </div>
-                <div className="form-outline mb-4">
-                    <input
-                        // onChange={handleUserName}
-                        required type="text"
-                        id="username"
-                        className="form-control form-control-lg"
-                        placeholder="Username"
-                        // value={username}
-                    />
-                </div>
-                <div className="form-outline mb-3">
-                    <input
-                        // onChange={(e) => setPassword(e.target.value)}
-                        required type="password"
-                        id="password"
-                        className="form-control form-control-lg"
-                        placeholder="Password"
-                        // value={password}
-                    />
-                </div>
-                <div className="d-flex justify-content-between align-items-center">
-                <div className="form-check mb-0">
-                    <input
-                        className="form-check-input me-2"
-                        type="checkbox"
-                        value=""
-                        id="remember"
-                    />
-                    <label className="form-check-label" htmlFor="remember">
-                        Remember me
-                    </label>
-                </div>
-                </div>
-                <div className="text-center text-lg mt-4 pt-2">
-                    <button
-                    type="button"
-                    className="btn btn-primary"
-                    style={{fontWeight:'bolder'}}
-                    // onClick={() => login(username, password)}
-                    >
-                    Sign Up
-                    </button>
-                </div>
-                </form>
+              <div className="form-outline mb-2">
+                <input
+                //   onChange={handleUserName}
+                  required
+                  type="text"
+                  id="username"
+                  className="form-control form-control-lg"
+                  placeholder="Username"
+                //   value={username}
+                />
+              </div>
+              <div className="form-outline mb-2">
+                <input
+                //   onChange={(e) => setPassword(e.target.value)}
+                  required
+                  type="password"
+                  id="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                //   value={password}
+                />
+              </div>
+              <div className="form-outline mb-2">
+                <input
+                //   onChange={(e) => setFirstName(e.target.value)}
+                  required
+                  type="text"
+                  id="first_name"
+                  className="form-control form-control-lg"
+                  placeholder="First Name"
+                //   value={first_name}
+                />
+              </div>
+              <div className="form-outline mb-2">
+                <input
+                //   onChange={(e) => setLastName(e.target.value)}
+                  required
+                  type="text"
+                  id="last_name"
+                  className="form-control form-control-lg"
+                  placeholder="Last Name"
+                //   value={last_name}
+                />
+              </div>
+              <div className="form-outline mb-2">
+                <input
+                //   onChange={(e) => setEmail(e.target.value)}
+                  required
+                  type="email"
+                  id="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email"
+                //   value={email}
+                />
+              </div>
+              <div className="text-center pt-2">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                //   onClick={() =>
+                //     signup(username, password, email, first_name, last_name)
+                //   }
+                >
+                  Sign Up
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Already on Wellness Pal?{" "}
+                  <a href="/user/login/" className="link-primary">
+                    Log in
+                  </a>
+                </p>
+              </div>
+            </form>
                   </Modal.Body>
                 </Modal>
               </div>
